fc4a083c5ed4682cda468a14f4b29a44527337bc
vikramkadu <vikramkkadu@gmail.com>
2024-03-03 00:38:06 +0530

    variable from config

---
+++ app/config/config.ts
@@ -0,0 +1,8 @@
+
+const API_BASE_URL = 'https://jsonplaceholder.typicode.com';
+const AVATAR_API_BASE_URL = 'https://api.dicebear.com/7.x/initials/svg?seed=';
+
+export const Config = {
+  apiBaseUrl: API_BASE_URL,
+  avatarApiBaseUrl: AVATAR_API_BASE_URL,
+};
